0325d164a5dc6621e1c82e33935a0a8b968c4bcf
Ilhama Novruzova <inovruzova07@gmail.com>
2023-12-21 06:41:22 +0400

    Finished Week 12 requirements

--- src/Note.js
+++ src/Note.js
@@ -3,8 +3,13 @@ import React from 'react';
 
 const Note = ({ title, content, createdDate, image }) => {
   const formattedDate = createdDate.toLocaleDateString();
+
+  function handleClick() {
+    alert(`Title: ${title}, Created on: ${formattedDate}`);
+  }
+  
   return (
-    <div className="note">
+    <div className="note" onClick={handleClick}>
       <h3>{title}</h3>
       <p>{content}</p>
       <p>{formattedDate}</p>
